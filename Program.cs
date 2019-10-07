e54e03a3a62e7a275d93d35ab0ef4918b8e0e611
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-10-07 14:20:48 -0500

    completed part 1 of exercise

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace CalculatorChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Calculatron 3000!");
+            Console.WriteLine("1) Add two numbers");
+            Console.WriteLine("2) Subtract two numbers");
+            Console.WriteLine("3) Multiply two numbers");
+            Console.WriteLine("4) Divide two numbers");
+            Console.WriteLine("0) Exit");
+            Console.WriteLine();
+            Console.Write("> ");
+
+            int menuSelection = int.Parse(Console.ReadLine());
+
+            while (menuSelection != 0)
+            {
+            Console.Write("Please enter the first number ");
+            double firstNumber = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the second number ");
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            double answer = 0;
+            string operation = "";
+
+            switch (menuSelection)
+            {
+                case 1:
+                    answer = firstNumber + secondNumber;
+                    operation = "adding";
+                    break;
+                case 2:
+                    answer = firstNumber - secondNumber;
+                    operation = "subtracting";
+                    break;
+                case 3:
+                    answer = firstNumber * secondNumber;
+                    operation = "multiplying";
+                    break;
+                case 4:
+                    answer = firstNumber / secondNumber;
+                    operation = "dividing";
+                    break;
+                case 0:
+                    break;
+            }
+            if (menuSelection != 0)
+            {
+                Console.WriteLine($"The result of {operation} {firstNumber} and {secondNumber} is {answer}");
+            }
+
+            Console.WriteLine("--------------------");
+            Console.Write("Choose another option> ");
+            menuSelection = int.Parse(Console.ReadLine());
+            }
+
+
+
+
+        }
+    }
+}
