83870785043de75f33c21d75d5ee49a0b051d058
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-10-09 14:06:05 -0500

    finished first phase

---
+++ Calculator.cs
@@ -0,0 +1,9 @@
+namespace CalculatorChallenge
+{
+    public class Calculator
+    {
+        public double Calculate(int menuSelection, double num1, double num2) {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,55 +13,68 @@ namespace CalculatorChallenge
             Console.WriteLine("4) Divide two numbers");
             Console.WriteLine("0) Exit");
             Console.WriteLine();
-            Console.Write("> ");
+            Console.Write("Choose a menu option> ");
 
-            int menuSelection = int.Parse(Console.ReadLine());
-
-            while (menuSelection != 0)
+            int menuSelection = -1;
+            while (menuSelection < 0)
             {
-            Console.Write("Please enter the first number ");
-            double firstNumber = double.Parse(Console.ReadLine());
+                try
+                {
+                    menuSelection = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Select a number from 0 to 4. Try again>");
+                    menuSelection = -1;
+                }
 
-            Console.Write("Please enter the second number ");
-            double secondNumber = double.Parse(Console.ReadLine());
+                while (menuSelection > 0)
+                {
+                    Console.Write("Please enter the first number ");
+                    double firstNumber = double.Parse(Console.ReadLine());
 
-            double answer = 0;
-            string operation = "";
+                    Console.Write("Please enter the second number ");
+                    double secondNumber = double.Parse(Console.ReadLine());
 
-            switch (menuSelection)
-            {
-                case 1:
-                    answer = firstNumber + secondNumber;
-                    operation = "adding";
-                    break;
-                case 2:
-                    answer = firstNumber - secondNumber;
-                    operation = "subtracting";
-                    break;
-                case 3:
-                    answer = firstNumber * secondNumber;
-                    operation = "multiplying";
-                    break;
-                case 4:
-                    answer = firstNumber / secondNumber;
-                    operation = "dividing";
-                    break;
-                case 0:
-                    break;
+
+                    double answer = 0;
+                    string operation = "";
+
+                    switch (menuSelection)
+                    {
+                        case 1:
+                            answer = firstNumber + secondNumber;
+                            operation = "adding";
+                            break;
+                        case 2:
+                            answer = firstNumber - secondNumber;
+                            operation = "subtracting";
+                            break;
+                        case 3:
+                            answer = firstNumber * secondNumber;
+                            operation = "multiplying";
+                            break;
+                        case 4:
+                            answer = firstNumber / secondNumber;
+                            operation = "dividing";
+                            break;
+                        case 0:
+                            break;
+                    }
+
+                    if (double.IsInfinity(answer))
+                    {
+                        Console.WriteLine("Dividing by zero is not allowed");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The result of {operation} {firstNumber} and {secondNumber} is {answer}");
+                    }
+
+                    menuSelection = -1;
+                    Console.Write("Choose another menu option> ");
+                }
             }
-            if (menuSelection != 0)
-            {
-                Console.WriteLine($"The result of {operation} {firstNumber} and {secondNumber} is {answer}");
-            }
-
-            Console.WriteLine("--------------------");
-            Console.Write("Choose another option> ");
-            menuSelection = int.Parse(Console.ReadLine());
-            }
-
-
-
-
         }
     }
 }
